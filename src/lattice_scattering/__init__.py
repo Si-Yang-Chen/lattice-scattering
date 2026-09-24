bd9c77c100f0cb42a42b14bdbbc931fb9b3f2702
sychen <chensiyang@ihep.ac.cn>
2026-09-25 03:55:46 +0800

    fix(plotting): list public plotting modules in package exports

--- src/lattice_scattering/__init__.py
+++ src/lattice_scattering/__init__.py
@@ -1,4 +1,4 @@
 """Finite-volume lattice scattering and amplitude analysis."""
 
 __version__ = "0.2.0.dev0"
-__all__ = ["core", "io", "symmetry", "amplitudes", "finite_volume", "fitting", "kinematics", "data"]
+__all__ = ["core", "io", "symmetry", "amplitudes", "finite_volume", "fitting", "kinematics", "data", "plotting", "plot_results"]
